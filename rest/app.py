e6fa1e8d4f916a652673e14002e65f1644d856f8
M. Yetisir <yetisir@gmail.com>
2020-04-30 10:08:12 -0700

    refactors tables

--- rest/app.py
+++ rest/app.py
@@ -6,8 +6,17 @@ from sqlalchemy.exc import OperationalError
 from config import app, connex_app, sql
 import tables
 
-if __name__ == '__main__':
-    sql.create_all()
-    event.listen(mapper, 'after_configured', tables.setup_schema)
+
+def main():
+    try:
+        sql.create_all()
+    except OperationalError:
+        app.logger.info('Unable to connect to database Trying again in 1s ...')
+        time.sleep(1)
+        main()
+
     connex_app.add_api('openapi.yml')
     connex_app.run(host='0.0.0.0', port=5001, debug=True)
+
+if __name__ == '__main__':
+    main()
--- rest/tables.py
+++ rest/tables.py
@@ -5,7 +5,24 @@ from sqlalchemy.orm import mapper
 from config import sql, ma
 
 
-class Borehole(sql.Model):
+def schema_metadata(cls):
+    class Meta:
+        model = cls
+        load_instance=True
+        sqla_session = sql.session
+        include_fk = True
+        include_relationships = True
+    return Meta
+
+
+class Table:
+    def __new__(cls, *args, **kwargs):
+        class Schema:
+            Meta = schema_metadata(cls)
+        cls.__marshmallow__ = Schema
+
+
+class Borehole(Table, sql.Model):
     borehole_id = sql.Column(sql.Integer(), primary_key=True)
     name = sql.Column(sql.String(64), nullable=False)
     easting = sql.Column(sql.Float(), nullable=False)
@@ -16,28 +33,11 @@ class Borehole(sql.Model):
 class Survey(sql.Model):
     survey_id = sql.Column(sql.Integer(), primary_key=True)
     borehole_id = sql.Column(
-            sql.Integer(), sql.ForeignKey('borehole.borehole_id'))
+            sql.Integer(), sql.ForeignKey(Borehole.borehole_id))
     depth = sql.Column(sql.Float(), nullable=False)
     survey_id = sql.Column(sql.Integer(), primary_key=True, nullable=False)
     azimuth = sql.Column(sql.Float(), nullable=False)
     dip = sql.Column(sql.Float(), nullable=False)
-
-
-class Lithology(sql.Model):
-    lithology_id = sql.Column(sql.Integer(), primary_key=True)
-    name = sql.Column(sql.String(64), nullable=False)
-
-
-class LithologyLog(sql.Model):
-    borehole_id = sql.Column(
-        sql.Integer(), sql.ForeignKey('borehole.borehole_id'),
-        primary_key=True)
-    depth_from = sql.Column(sql.Float(), primary_key=True)
-    depth_to = sql.Column(sql.Float(), nullable=False)
-    lithology_id = sql.Column(
-            sql.Integer(), sql.ForeignKey('lithology.lithology_id'),
-            nullable=False)
-    comments = sql.Column(sql.String(1024))
 
 
 class CorePhotoCorner(sql.Model):
@@ -50,7 +50,7 @@ class CorePhotoCorner(sql.Model):
 class CorePhotoLog(sql.Model):
     hash = sql.Column(sql.Integer, primary_key=True)
     borehole_id = sql.Column(sql.Integer(),
-        sql.ForeignKey('borehole.borehole_id'), nullable=False)
+        sql.ForeignKey(Borehole.borehole_id), nullable=False)
     depth_from = sql.Column(sql.Float(), primary_key=True)
     depth_to = sql.Column(sql.Float(), nullable=False)
     path = sql.Column(sql.String(1024), nullable=False)
@@ -67,32 +67,3 @@ class CorePhotoLog(sql.Model):
         sql.Integer(), sql.ForeignKey(CorePhotoCorner.corner_id),
         nullable=False)
     comments = sql.Column(sql.String(1024))
-
-
-def generate_sql(sql_file_path):
-    sql_queries = []
-
-    for table in sql.Model.metadata.tables.values():
-        table_sql = CreateTable(table).compile(dialect=postgresql.dialect())
-        sql_queries.append(str(table_sql).strip() + ';')
-
-    with open(sql_file_path, 'w') as sql_file:
-        sql_file.write('-- Automatically generated queries from tables.py\n')
-        sql_file.write('\n\n'.join(sql_queries))
-
-
-def setup_schema():
-    for cls in sql.Model._decl_class_registry.values():
-        if not hasattr(cls, '__tablename__'):
-            continue
-
-        class Meta:
-            model = cls
-            sqla_session = sql.session
-            load_instance=True
-
-        schema_class_name = f'{cls.__name__}Schema'
-        schema_class = type(
-            schema_class_name, (ma.SQLAlchemyAutoSchema, ), {'Meta': Meta})
-
-        setattr(cls, '__marshmallow__', schema_class)
