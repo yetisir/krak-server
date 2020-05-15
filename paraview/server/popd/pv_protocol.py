2faa1227f7ae192f1971354d57cf0408fb7b3355
M. Yetisir <yetisir@gmail.com>
2020-05-14 18:04:08 -0700

    adds simple vtk rendering client/server

---
+++ paraview/server/popd/pv_protocol.py
@@ -0,0 +1,107 @@
+import os, time
+
+from wslink import register as exportRpc
+
+from paraview import simple, servermanager
+from paraview.web import protocols as pv_protocols
+
+try:
+    # PV 5.8+
+    from paraview.modules.vtkRemotingViews import vtkPVRenderView
+except:
+    pass
+
+try:
+    # PV 5.7
+    from paraview.modules.vtkPVClientServerCoreRendering import vtkPVRenderView
+except:
+    pass
+
+try:
+    # PV 5.6
+    from vtkmodules.vtkPVClientServerCoreRendering import vtkPVRenderView
+except:
+    pass
+
+# Make sure only one cone is available
+cone = simple.Cone()
+
+class ParaViewCone(pv_protocols.ParaViewWebProtocol):
+
+    def getCamera(self):
+        view = self.getView('-1')
+        bounds = [-1, 1, -1, 1, -1, 1]
+
+        if view and view.GetClientSideView().GetClassName() == 'vtkPVRenderView':
+          rr = view.GetClientSideView().GetRenderer()
+          bounds = rr.ComputeVisiblePropBounds()
+
+        return {
+          'id': view.GetGlobalIDAsString(),
+          'bounds': bounds,
+          'position': tuple(view.CameraPosition),
+          'viewUp': tuple(view.CameraViewUp),
+          'focalPoint': tuple(view.CameraFocalPoint),
+          'centerOfRotation': tuple(view.CenterOfRotation),
+        }
+
+
+    @exportRpc("vtk.initialize")
+    def createVisualization(self):
+        simple.Show(cone)
+        return self.resetCamera()
+
+
+    @exportRpc("vtk.camera.reset")
+    def resetCamera(self):
+        view = self.getView('-1')
+        simple.Render(view)
+        simple.ResetCamera(view)
+        try:
+            view.CenterOfRotation = view.CameraFocalPoint
+        except:
+            pass
+
+        self.getApplication().InvalidateCache(view.SMProxy)
+        self.getApplication().InvokeEvent('UpdateEvent')
+
+        return self.getCamera()
+
+
+    @exportRpc("vtk.cone.resolution.update")
+    def updateResolution(self, resolution):
+        cone.Resolution = resolution
+        self.getApplication().InvokeEvent('UpdateEvent')
+
+
+    @exportRpc("viewport.mouse.zoom.wheel")
+    def updateZoomFromWheel(self, event):
+      if 'Start' in event["type"]:
+        self.getApplication().InvokeEvent('StartInteractionEvent')
+
+      viewProxy = self.getView(event['view'])
+      if viewProxy and 'spinY' in event:
+        rootId = viewProxy.GetGlobalIDAsString()
+        zoomFactor = 1.0 - event['spinY'] / 10.0
+
+        if rootId in self.linkedViews:
+          fp = viewProxy.CameraFocalPoint
+          pos = viewProxy.CameraPosition
+          delta = [fp[i] - pos[i] for i in range(3)]
+          viewProxy.GetActiveCamera().Zoom(zoomFactor)
+          viewProxy.UpdatePropertyInformation()
+          pos2 = viewProxy.CameraPosition
+          viewProxy.CameraFocalPoint = [pos2[i] + delta[i] for i in range(3)]
+          dstViews = [self.getView(vid) for vid in self.linkedViews]
+          pushCamera(viewProxy, dstViews)
+        else:
+          fp = viewProxy.CameraFocalPoint
+          pos = viewProxy.CameraPosition
+          delta = [fp[i] - pos[i] for i in range(3)]
+          viewProxy.GetActiveCamera().Zoom(zoomFactor)
+          viewProxy.UpdatePropertyInformation()
+          pos2 = viewProxy.CameraPosition
+          viewProxy.CameraFocalPoint = [pos2[i] + delta[i] for i in range(3)]
+
+      if 'End' in event["type"]:
+        self.getApplication().InvokeEvent('EndInteractionEvent')
---
+++ vue/src/store/network.js
@@ -0,0 +1,82 @@
+import vtkWSLinkClient from 'vtk.js/Sources/IO/Core/WSLinkClient';
+import SmartConnect from 'wslink/src/SmartConnect';
+
+import coneProtocol from 'vue-vtkjs-pvw-template/src/io/protocol';
+
+import { Mutations } from 'vue-vtkjs-pvw-template/src/store/TYPES';
+
+// Bind vtkWSLinkClient to our SmartConnect
+vtkWSLinkClient.setSmartConnectClass(SmartConnect);
+
+export default {
+  state: {
+    client: null,
+    config: null,
+  },
+  getters: {
+    NETWORK_CLIENT(state) {
+      return state.client;
+    },
+    NETWORK_CONFIG(state) {
+      return state.config;
+    },
+  },
+  mutations: {
+    NETWORK_CLIENT_SET(state, client) {
+      state.client = client;
+    },
+    NETWORK_CONFIG_SET(state, config) {
+      state.config = config;
+    },
+  },
+  actions: {
+    NETWORK_CONNECT({ commit, state }) {
+      const { config, client } = state;
+      if (client && client.isConnected()) {
+        client.disconnect();
+      }
+      let clientToConnect = client;
+      if (!clientToConnect) {
+        clientToConnect = vtkWSLinkClient.newInstance();
+        clientToConnect.setProtocols({
+          Cone: coneProtocol,
+        });
+      }
+
+      // Connect to busy store
+      clientToConnect.onBusyChange((count) => {
+        commit(Mutations.BUSY_COUNT_SET, count);
+      });
+      clientToConnect.beginBusy();
+
+      // Error
+      clientToConnect.onConnectionError((httpReq) => {
+        const message =
+          (httpReq && httpReq.response && httpReq.response.error) ||
+          `Connection error`;
+        console.error(message);
+        console.log(httpReq);
+      });
+
+      // Close
+      clientToConnect.onConnectionClose((httpReq) => {
+        const message =
+          (httpReq && httpReq.response && httpReq.response.error) ||
+          `Connection close`;
+        console.error(message);
+        console.log(httpReq);
+      });
+
+      // Connect
+      clientToConnect
+        .connect(config)
+        .then((validClient) => {
+          commit(Mutations.NETWORK_CLIENT_SET, validClient);
+          clientToConnect.endBusy();
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
