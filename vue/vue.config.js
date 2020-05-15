cb70be2a13efbfa81e2f65739c6f510e00c48b29
M. Yetisir <yetisir@gmail.com>
2020-05-15 11:35:25 -0700

    minor update

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -3,8 +3,6 @@ const vtkChainWebpack = require('vtk.js/Utilities/config/chainWebpack');
 module.exports = {
   chainWebpack: (config) => {
     // Add project name as alias
-    //config.resolve.alias.set('vue-vtkjs-pvw-template', __dirname);
-
     config.resolve.alias.set('@', [__dirname, 'src'].join('/'));
     // Add vtk.js rules
     vtkChainWebpack(config);
